bd3b7ac0e68225a6dbdd6e56eb56c689a28e97df
Степан Рудаков <19stepan94@mail.ru>
2024-08-07 21:42:47 +0500

    получил элементы из модального окна в константы, добавил класс для модального окна который меняет свойство display

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const modalTitle = document.querySelectorAll('modal__title');
+const modalVendorCode = document.querySelectorAll('vendor-code__wrapper');
+const modalVendorCodeId = document.querySelectorAll('vendor-code__id');
+const modalForm = document.querySelectorAll('modal__form');
+const modalCheckbox = document.querySelectorAll('modal__checkbox');
+const modalnputdiscount = document.querySelectorAll('modal__input_discount');
+const modaTotalPrice = document.querySelectorAll('modal__total-price');
+
