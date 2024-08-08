628167e64eec70f7163e56bc37901754008f7990
Степан <19stepan94@mail.ru>
2024-08-08 10:11:59 +0500

    поправил подключение файла js и получение элементов в константы, добавил eslint

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-'use strict'
+'use strict';
 
-const modalTitle = document.querySelectorAll('modal__title');
-const modalVendorCode = document.querySelectorAll('vendor-code__wrapper');
-const modalVendorCodeId = document.querySelectorAll('vendor-code__id');
-const modalForm = document.querySelectorAll('modal__form');
-const modalCheckbox = document.querySelectorAll('modal__checkbox');
-const modalnputdiscount = document.querySelectorAll('modal__input_discount');
-const modaTotalPrice = document.querySelectorAll('modal__total-price');
+const modalTitle = document.querySelectorAll('.modal__title');
+const modalVendorCode = document.querySelectorAll('.vendor-code__wrapper');
+const modalVendorCodeId = document.querySelectorAll('.vendor-code__id');
+const modalForm = document.querySelectorAll('.modal__form');
+const modalCheckbox = document.querySelectorAll('.modal__checkbox');
+const modalnputdiscount = document.querySelectorAll('.modal__input_discount');
+const modaTotalPrice = document.querySelectorAll('.modal__total-price');
+const modalDisplayFlex = document.querySelectorAll('.active');
+
+console.log(modalTitle);
+console.log(modalVendorCode);
+console.log(modalVendorCodeId);
+console.log(modalForm);
+console.log(modalCheckbox);
+console.log(modalnputdiscount);
+console.log(modaTotalPrice);
+console.log(modalDisplayFlex);
+
+modalDisplayFlex[0].remove();
 
